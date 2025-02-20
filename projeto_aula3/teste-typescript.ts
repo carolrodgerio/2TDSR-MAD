8f3050831bc3d5504940b0ee0f1291e456d9ec95
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-19 23:01:25 -0300

    // create 250219-Typescript

---
+++ projeto_aula3/teste-typescript.ts
@@ -0,0 +1,14 @@
+let a : number;
+a = 100;
+
+console.log("A: ", a)
+
+// ARRAY:
+
+let pares : number[] = [0, 2, 4, 6, 8]
+let impares : Array<number> = [1, 3, 5, 7, 9]
+
+// OBJETO:
+
+let pessoa1 : {nome : string, idade : number, telefone : string, cpf : string}
+pessoa1 = {nome: "João Silva", idade: 25, telefone: "(11) 99999-9999", cpf: "111111111-11"}
