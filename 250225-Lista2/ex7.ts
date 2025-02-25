d6b26e9e8dd7cc180231b63c1abe06505794aa75
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-25 12:24:49 -0300

    // create ex7.ts

---
+++ 250225-Lista2/ex7.ts
@@ -0,0 +1,11 @@
+function quadro(faculdade: string, programa: string, autor: string) {
+
+    console.log("#".repeat(30));
+    console.log("# Nome da faculdade: ", faculdade, " #");
+    console.log("# Nome do programa: ", programa, " #");
+    console.log("# Feito por: ", autor, " #");
+    console.log("#".repeat(30));
+
+}
+
+quadro("FIAP", "Análise e Desenvolvimento de Sistemas", "Carolina Estevam Rodgerio");
