a5b5ab3e276c2dab959eb8fa4afd6351af5d012f
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-25 12:25:34 -0300

    // create ex9.ts

---
+++ 250225-Lista2/ex9.ts
@@ -0,0 +1,12 @@
+let consumo1: number = 230;
+let consumo2: number = 120;
+let consumo3: number = 350;
+let consumo4: number = 410;
+let consumo5: number = 70;
+let valorKVh: number = 0.5;
+
+console.log("Casa nº 14     KV/h: ", consumo1, "R$: ", consumo1 * valorKVh);
+console.log("Casa nº 35     KV/h: ", consumo2, "R$: ", consumo2 * valorKVh);
+console.log("Casa nº 54     KV/h: ", consumo3, "R$: ", consumo3 * valorKVh);
+console.log("Casa nº 71     KV/h: ", consumo4, "R$: ", consumo4 * valorKVh);
+console.log("Casa nº 92     KV/h: ", consumo5, "R$: ", consumo5 * valorKVh);
