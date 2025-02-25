3871f892ac095f0b18e53b5dcebab187e51adb54
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-25 12:23:55 -0300

    // create ex5.ts

---
+++ 250225-Lista2/ex5.ts
@@ -0,0 +1,6 @@
+let skeep: {energia: number, velocidade: number, temperatura: number, humor: number};
+skeep = {energia: 75, velocidade: 60, temperatura: 35, humor: 90};
+console.log("Energia ==> ", skeep.energia);
+console.log("Velocidade ==> ", skeep.velocidade);
+console.log("Temperatura ==> ", skeep.temperatura);
+console.log("Humor ==> ", skeep.humor);
