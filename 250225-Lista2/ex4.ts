3721a28b962df06a1d98e711c0f45306e9466e5c
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-25 12:23:33 -0300

    // create ex4.ts

---
+++ 250225-Lista2/ex4.ts
@@ -0,0 +1,25 @@
+// a) const
+const nomeEmpresa: string = "Amazon";
+
+// b) let
+let totalSalarios: number = 50;
+
+// c) const
+const diasFaturamento: number = 22;
+
+// d) let
+let notas: {prova1: number, prova2: number, prova3: number, prova4: number};
+notas = {prova1: 9.5, prova2: 6.75, prova3: 8, prova4: 7.25};
+
+// e) const
+const dadosCarro: {placa: string, chassi: number, modelo: string, ano: number; cor: string; nomeDono: string} = {placa: "ABC1234", chassi: 9876543210, modelo: "Renault Kwid", ano: 2015, cor: "magenta", nomeDono: "Enrico Andrade"};
+
+// f) const
+const numeroOuro: number = 1.61803;
+
+// g) let
+let alunos: {aluno1: string, aluno2: string, aluno3: string, aluno4: string, aluno5: string, aluno6: string, aluno7: string, aluno8: string, aluno9: string, aluno10: string};
+alunos = {aluno1: "Carolina", aluno2: "Lucas", aluno3: "Enrico", aluno4: "Ruan", aluno5: "Guilherme", aluno6: "Ana", aluno7: "Gabriel", aluno8: "Denise", aluno9: "Mariana", aluno10: "Angela"}
+
+// h) let
+let paresTenis: number = 8;
