f2d7f1acb39d7dbdacb4f530a9cdf73ef0067646
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-02-25 12:22:32 -0300

    // create ex1.ts

---
+++ 250225-Lista2/ex1.ts
@@ -0,0 +1,14 @@
+let faculdade: string;
+let curso: string;
+let ra: number;
+let nome: string;
+
+faculdade = "FIAP";
+curso = "Análise e Desenvolvimento de Sistemas";
+ra = 554975;
+nome = "Carolina Estevam Rodgerio";
+
+console.log("Nome da faculdade: ", faculdade);
+console.log("Curso: ", curso);
+console.log("RA: ", ra);
+console.log("Nome: ", nome);
